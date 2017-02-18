091b941f0f4e96c368e2c5d61c226338f9bc0188
Rui Li <cynicalrui@gmail.com>
2017-02-18 13:49:53 -0500

    Added average RGB algorithm and RGB class

--- Picture.java
+++ Picture.java
@@ -2,8 +2,15 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import java.awt.*;
 
 public class Picture {
+	static int scalex;
+	static int scaley;
+	public Picture(int sx, int sy){
+		scalex = sx;
+		scaley = sy;
+	}
 	public static int[][] convertTo2DUsingGetRGB(BufferedImage image) {
       int width = image.getWidth();
       int height = image.getHeight();
@@ -14,5 +21,38 @@ public class Picture {
          }
       }
       return result;
+   }
+   public static RGB[][] getRGBAverages(int[][] rgb){
+   		RGB[][] result = new RGB[rgb.length/scaley][rgb[0].length/scalex];
+   		int redAverage = 0;
+   		int greenAverage = 0;
+   		int blueAverage = 0;
+   		int idx = 0;
+   		int idy = 0;
+   		for(int i = 0; i < rgb.length/scaley; i++){
+   			for(int j = 0; j < rgb[i].length/scalex; j++){
+   				for(int y = 0; y <  scaley; y++){
+   					for(int x = 0; x < scalex; x++){
+		   				Color c = new Color(rgb[idy+y][idx+x]);
+		   				redAverage += c.getRed();
+		   				blueAverage += c.getBlue();
+		   				greenAverage += c.getGreen();
+	   				}
+   				}
+   				idx += scalex;
+   				result[i][j] = new RGB(redAverage, blueAverage, greenAverage);
+   			}
+   			idy += scaley;
+   			idx = 0;
+   		}
+   		for(int i = 0; i < result.length; i++){
+   			for(int j = 0; j < result[i].length; j++){
+   				System.out.println(result[i][j].red + " " + result[i][j].blue + " " + result[i][j].green);
+   				result[i][j].red = result[i][j].red / (scalex * scaley);
+   				result[i][j].blue = result[i][j].blue / (scalex * scaley);
+   				result[i][j].green = result[i][j].green / (scalex * scaley);
+   			}
+   		}
+   		return result;
    }
 }
