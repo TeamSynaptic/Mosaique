79037f27681dbd1f52d35aa1073a9ca9b166ac9d
Rui Li <cynicalrui@gmail.com>
2017-02-18 15:57:14 -0500

    Added comments, and fixed some OOP bugs

--- RGB.java
+++ RGB.java
@@ -1,11 +1,29 @@
 public class RGB {
-	int red;
-	int blue;
-	int green;
+	private int red;
+	private int blue;
+	private int green;
 	public RGB(int r, int b, int g){
 		red = r;
 		blue = b;
 		green = g;
 	}
 	public RGB(){}
+	public int getRed(){
+		return red;
+	}
+	public int getBlue(){
+		return blue;
+	}
+	public int getGreen(){
+		return green;
+	}
+	public void setRed(int r){
+		red = r;
+	}
+	public void setBlue(int b){
+		blue = b;
+	}
+	public void setGreen(int g){
+		green = g;
+	}
 }
